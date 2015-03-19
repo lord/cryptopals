72163adf9a1e55143ae371861c28201960ce0014
Robert Lord <robert@lord.io>
2015-03-19 14:41:54 -0400

    Add test for challange 2

--- set1.go
+++ set1.go
@@ -16,6 +16,10 @@ func hexToBase64(hexString string) string {
 	return b64.EncodeToString(str)
 }
 
+func xor(one, two []byte) []byte {
+	return []byte{}
+}
+
 func main() {
 	fmt.Print(hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
 }
--- set1_test.go
+++ set1_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"testing"
 )
 
@@ -9,5 +10,14 @@ func TestCh1(t *testing.T) {
 	answer := "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 	if hexToBase64(str) != answer {
 		t.Error("Challange 1 Failed")
+	}
+}
+
+func TestCh2(t *testing.T) {
+	str1, _ := hex.DecodeString("1c0111001f010100061a024b53535009181c")
+	str2, _ := hex.DecodeString("686974207468652062756c6c277320657965")
+	answer, _ := hex.DecodeString("746865206b696420646f6e277420706c6179")
+	if xor(str1, str2) != answer {
+		t.Error("Challange 2 Failed")
 	}
 }
