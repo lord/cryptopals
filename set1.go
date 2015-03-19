f483c1c7f4ef0c85e3d42d44bbb6b6c494898e8f
Robert Lord <robert@lord.io>
2015-03-19 14:30:51 -0400

    Add solution to first problem

---
+++ set1.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+var b64 = base64.StdEncoding
+
+func hexToBase64(hexString string) string {
+	str, err := hex.DecodeString(hexString)
+	if err != nil {
+		panic(err)
+	}
+	return b64.EncodeToString(str)
+}
+
+func main() {
+	fmt.Print(hexToBase64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"))
+}
